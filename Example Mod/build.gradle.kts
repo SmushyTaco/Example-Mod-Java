999a000a3320aea5f414a2a169887fd3e5bdedc6
SmushyTaco <41648290+SmushyTaco@users.noreply.github.com>
2022-12-16 15:19:01 -0800

    Update build.gradle.kts

--- Example Mod/build.gradle.kts
+++ Example Mod/build.gradle.kts
@@ -17,7 +17,7 @@ tasks {
         targetCompatibility = javaVersion.toString()
         options.release.set(javaVersion.toString().toInt())
     }
-    jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
+    jar { from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } } }
     processResources {
         filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.extra["mod_version"] as String, "fabricloader" to project.extra["loader_version"] as String, "fabric_api" to project.extra["fabric_version"] as String, "minecraft" to project.extra["minecraft_version"] as String, "java" to project.extra["java_version"] as String)) }
         filesMatching("*.mixins.json") { expand(mutableMapOf("java" to project.extra["java_version"] as String)) } 

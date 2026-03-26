979e37a87096b5da853322b2145e90eec314e942
Nikan Radan <github.com@alias.nikanradan.com>
2026-03-26 13:16:39 -0700

    26.1

--- Example Mod/build.gradle.kts
+++ Example Mod/build.gradle.kts
@@ -16,9 +16,8 @@ version = modVersion.get()
 group = mavenGroup.get()
 dependencies {
     minecraft(libs.minecraft)
-    mappings(loom.officialMojangMappings())
-    modImplementation(libs.loader)
-    modImplementation(libs.fabric.api)
+    implementation(libs.loader)
+    implementation(libs.fabric.api)
 }
 java {
     toolchain {
@@ -84,7 +83,7 @@ tasks {
         group = "publishing"
         disableVersionDetection()
         apiToken = env.fetch("CURSEFORGE_TOKEN", "")
-        val file = upload("Replace this with the CurseForge project ID as an Integer", remapJar)
+        val file = upload("Replace this with the CurseForge project ID as an Integer", jar)
         file.displayName = "[${libs.versions.minecraft.get()}] Mod Name"
         file.addEnvironment("Client", "Server")
         file.changelog = ""
@@ -96,7 +95,7 @@ tasks {
 modrinth {
     token = env.fetch("MODRINTH_TOKEN", "")
     projectId = "Replace this with the slug to the Modrinth mod page"
-    uploadFile.set(tasks.remapJar)
+    uploadFile.set(tasks.jar)
     gameVersions.add(libs.versions.minecraft)
     versionName = libs.versions.minecraft.map { "[$it] Mod Name" }
     dependencies { required.project("fabric-api") }
